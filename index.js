e817e1e12b4ee70d7542f528baff0ae6d4c6e2ae
ghaldazhra <ghaldazhra1@gmail.com>
2026-02-24 13:45:30 +0700

    greeting + middleware

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from 'express';
 
 const app = express();
+
+app.use((req, res, next) => {       // Middleware untuk mencetak path setiap kali ada permintaan masuk //
+  console.log(`req baruu: ${req.path}`); 
+  next();
+});
+
 const port = 3000;  
 app.get('/', (req, res) => {
   res.send('Hello Ghalda!');
 });
+
+app.get('/say/:greeting', (req, res) => {
+    const { greeting } = req.params;
+    res.send(greeting);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
