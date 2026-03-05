6b33363ae57c93fe68deb54468f9a6d4d5f93cd1
ghaldazhra <ghaldazhra1@gmail.com>
2026-03-05 15:29:40 +0700

    notification 2

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,14 +1,14 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
-const transactionSchema = new mongoose.Schema({
-    orderId: { type: String, required: true, unique: true },
-    gross_amount: { type: Number, required: true },
-    first_name: String,
-    email: String,
-    transaction_status: { type: String, default: 'pending' },   
-    payment_type: String,
-}, 
-{ timestamps: true }
-);
+// const transactionSchema = new mongoose.Schema({
+//     orderId: { type: String, required: true, unique: true },
+//     gross_amount: { type: Number, required: true },
+//     first_name: String,
+//     email: String,
+//     transaction_status: { type: String, default: 'pending' },   
+//     payment_type: String,
+// }, 
+// { timestamps: true }
+// );
 
-export default mongoose.model('Transaction', transactionSchema);
+// export default mongoose.model('Transaction', transactionSchema);
